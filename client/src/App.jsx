d76bc86cd446178d278ee6cb3c08692704091803
DevRidmi <guinevere1998in@gmail.com>
2024-03-31 16:06:44 +0530

    add components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Login from './components/LoginPage'
+import Register from './components/Register'
+import PageNotFound from './components/PageNotFound'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<div>Home</div>
+    },
+    {
+      path: "/login",
+      element:<Login/>
+    },
+    {
+      path: "/register",
+      element:<Register/>
+    },
+    {
+      path: "*",
+      element:<PageNotFound/>
+    },
+  ])
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   )
 }
 
