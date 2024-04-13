f59baef51049045a021caeb22afbadf1ed0d2787
DevRidmi <guinevere1998in@gmail.com>
2024-04-13 20:10:55 +0530

    add song row

--- client/src/components/AlbumSongLists.jsx
+++ client/src/components/AlbumSongLists.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import SpotifyWebApi from 'spotify-web-api-js'
 import { useStateValue } from '../StateProvider'
 import { cTrack } from '../dummyData/dummy'
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline'
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded'
 
 function AlbumSongLists(trackID) {
 
@@ -36,12 +36,20 @@ function AlbumSongLists(trackID) {
     console.log(track)
     return (
         <div className="w-full overflow-hidden max-md:px-0 flex flex-col gap-2 text-white px-2.1">
-            <div className='p-2 m-2 rounded-lg flex 'style={{ backgroundColor: 'rgba(169,169,169,0.05)' }}>
-                <img src={track.album.images[0].url} alt="" className='w-14 h-14 rounded-sm' />
-                <h4>{track.name}</h4>
-                <h4>{millisToMinutesAndSeconds(track.duration_ms)}</h4>
-                <PlayCircleOutlineIcon />
+            <div className='p-3 m-2 rounded-lg flex items-center justify-between' style={{ backgroundColor: 'rgba(169,169,169,0.05)' }}>
+                <div className="flex items-center space-x-4">
+                    <img src={track.album.images[0].url} alt="" className='w-14 h-14 rounded-sm' />
+                    <div>
+                        <h4 className="text-base font-semibold">{track.name}</h4>
+                        <h6 className="text-sm text-gray-500">{track.album.artists[0].name}</h6>
+                    </div>
+                </div>
+                <div className="flex items-center space-x-4">
+                    <h4 className="text-base font-semibold">{millisToMinutesAndSeconds(track.duration_ms)}</h4>
+                    <div className='text-xl'><PlayArrowRoundedIcon /></div>
+                </div>
             </div>
+
         </div>
     )
 }
--- client/src/dummyData/dummy.js
+++ client/src/dummyData/dummy.js
@@ -1,21 +1,23 @@
 export const currentAlbum = {
     images: [
-        { url: 'https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg'}
+        { url: 'https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg' }
     ],
     name: "name",
     label: "label",
     release_date: "release_date",
     total_tracks: "total_tracks",
-    tracks:{
-        items:[
+    tracks: {
+        items: [
             "item1"
         ]
     }
 }
 
 export const cTrack = {
-    album:{
-        images:[{url:""}]},
-        name:"",
-        duration_ms:""
+    album: {
+        images: [{ url: "https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg" }],
+        artists:[{name:""}]
+    },
+    name: "",
+    duration_ms: ""
 }
